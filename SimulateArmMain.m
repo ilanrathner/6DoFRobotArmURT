ec82da5113cdb728e6b7cdcd3aaff948eb5f2004
ilanrathner <ilan.rathner@gmail.com>
2025-08-07 13:19:08 +1000

    trying to make a pointcloud of reachable workspace thats easy to read

--- SimulateArmMain.m
+++ SimulateArmMain.m
@@ -2,22 +2,21 @@ linklengths = [10;10;10;10;10];
 thetas = [90,90,0,180,90,90] * pi/180;
 dh = createDHTable(linklengths, thetas);
 
+%testing inverse kinematics
 %T = transMax(0,7,dh);
 %R = T(1:3,1:3);
 %[yaw,pitch,roll] = inverseOrientation(R)
-
 %armSim = initialiseArm(dh);
-
-[t1,t2,t3,t4,t5,t6] = inverseKinematics(0,20,30, 0,0,0, linklengths);
-ts = [t1,t2,t3,t4,t5,t6] * 180/pi
+% [t1,t2,t3,t4,t5,t6] = inverseKinematics(0,20,30, 0,0,0, linklengths);
+% ts = [t1,t2,t3,t4,t5,t6] * 180/pi;
 
 % A board facing X-axis, centered at x=15, z=10
-drawVerticalBoard('x', 30, 30, 60, 90, 10);
-
-hold on
+%drawVerticalBoard('x', 30, 30, 60, 90, 10);
 
 armSim = initialiseArm(dh);
-[linklengths, thetas] = controlArmSimulation(linklengths, thetas, armSim);
+%[linklengths, thetas] = controlArmSimulation(linklengths, thetas, armSim);
+hold on
+plotReachableWorkspaceCombos(linklengths);
 
 %inputs are in cm
 function drawVerticalBoard(axisDirection, axisOffset, baseHeight, boardWidth, boardHeight, gridSpacing)
